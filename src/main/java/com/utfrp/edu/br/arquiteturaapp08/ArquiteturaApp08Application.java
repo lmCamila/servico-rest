22ed1e2a3d7e3040462a85060bb3889eb0e99827
lmCamila <camilalm31@gmail.com>
2018-11-06 13:02:12 -0200

    Criado projeto para expor dao ficticio.

---
+++ src/main/java/com/utfrp/edu/br/arquiteturaapp08/ArquiteturaApp08Application.java
@@ -0,0 +1,12 @@
+package com.utfrp.edu.br.arquiteturaapp08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaApp08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaApp08Application.class, args);
+	}
+}
---
+++ src/test/java/com/utfrp/edu/br/arquiteturaapp08/ArquiteturaApp08ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.utfrp.edu.br.arquiteturaapp08;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArquiteturaApp08ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
